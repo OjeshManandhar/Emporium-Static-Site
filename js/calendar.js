8229ac9a130d8717235a3a7cdfa7b777a65bef32
OjeshManandhar <ozes.manandhar@gmail.com>
2019-08-21 18:15:34 +0545

    Complete news listings JS part

--- js/calendar.js
+++ js/calendar.js
@@ -66,7 +66,7 @@ function removeCalendar() {
     var calendarContainer;
 
     for (var i = 0; i < newsCalendar.childNodes.length; i++) {
-        if (newsCalendar.childNodes[i].className == "calendar-container") {
+        if (newsCalendar.childNodes[i].className === "calendar-container") {
             calendarContainer = newsCalendar.childNodes[i];
           break;
         }        
@@ -100,6 +100,9 @@ function renderCalendar(date = new Date()) {
 
         removeCalendar();
         renderCalendar(prevMon);
+
+        removeNewsList();
+        renderNewsList(prevMon);
     });
 
     const curYear = monthYear.appendChild(document.createElement('dev'));
@@ -125,6 +128,9 @@ function renderCalendar(date = new Date()) {
 
         removeCalendar();
         renderCalendar(nextMon);
+
+        removeNewsList();
+        renderNewsList(nextMon);
     });
 
     // table
@@ -201,8 +207,7 @@ function renderCalendar(date = new Date()) {
         day.id = id;
         day.addEventListener('click', function(e) {
             // alert(findNewsOnDate(this.id)? findNewsOnDate(this.id) : "No news");
-            removeNewsList();
-            renderNewsList(this.id);
+            showNewsOfDate(this.id);
         });
     }
 }
@@ -211,39 +216,105 @@ function removeNewsList() {
     const parent = document.getElementById('news-listings');
     var child;
 
-    
     for (var i = 0; i < parent.childNodes.length; i++) {
-        if (parent.childNodes[i].className == 'news-detail') {
+        if (
+            parent.childNodes[i].className === 'no-news' || 
+            parent.childNodes[i].className === 'news-list'
+
+        ) {
             child = parent.childNodes[i];
-          break;
+            break;
         }        
     }
 
     parent.removeChild(child);
 }
 
-function renderNewsList(date = null) {
-    const news = document.getElementById('news-listings').appendChild(document.createElement('div'));
-    news.className = 'news-detail';
+function showNewsOfDate(id) {
+    const newsId = id.replace('-cal', '-news');
 
-    if (date === null) {
-        date = generateId(new Date(), new Date().getDate());
+    // No news for the current month i.e. no div with class 'news-list'
+    if (document.getElementsByClassName('news-list').length === 0) {
+        return;
     }
-    
-    date = date.replace('-cal', '');
 
-    news.innerHTML = 'No news for <b>' + date + '</b>';
+    var parent = document.getElementById(newsId);
+    var child;
 
-    for (var key in newsEvents) {
-        if (key === date) {
-            news.innerHTML = 'New for <b>' + date + '</b> <br /> <br />' + 
-                '<b>' + newsEvents[key][0] + '</b>' +
-                '<br /> <br />' + newsEvents[key][1];
+    // Disable already active main-news
+    child = document.getElementsByClassName('main-news-active');
+    if (child.length !== 0) {
+        child[0].classList.replace('main-news-active', 'main-news-inactive');
+    }
+
+    // Making selected date's news active
+    if (parent === null) {
+        return;
+    }
+
+    for (var i = 0; i < parent.childNodes.length; i++) {
+        if (parent.childNodes[i].classList.contains('main-news-inactive')) {
+            child = parent.childNodes[i];
             break;
         }
     }
+    child.classList.replace('main-news-inactive', 'main-news-active');
 }
 
-// createCalendar(new Date());
+function renderNewsList(date = new Date()) {
+    const newsList = [];
+    var monthStr = `${date.getFullYear()}-`;
+
+    if ((date.getMonth() + 1) < 10) {
+        monthStr += `0${date.getMonth() + 1}`;
+    } else {
+        monthStr += `${date.getMonth() + 1}`;
+    }
+
+    for (var key in newsEvents) {
+        if (key.indexOf(monthStr) > -1) {
+            newsList.push({
+                key: key,
+                news: [newsEvents[key][0], newsEvents[key][1]]
+            });
+        }
+    }
+
+    const news = document.getElementById('news-listings').appendChild(document.createElement('div'));
+
+    if (newsList.length === 0) {
+        // console.log('No news Found');
+
+        news.className = 'no-news';
+        news.innerHTML = `No news for ${monthName(date)}`; 
+    } else {
+        // console.log('newsList:', newsList);
+        
+        news.className = 'news-list';
+
+        for (let i = 0; i < newsList.length; i++) {
+            let newsSingle = news.appendChild(document.createElement('div'));
+            newsSingle.id = `${newsList[i].key}-news`;
+            newsSingle.className = 'news-single';
+            // newsSingle.innerHTML = newsList[i].key;
+
+            const heading = newsSingle.appendChild(document.createElement('div'));
+            heading.className = 'heading';
+            heading.innerHTML = newsList[i].news[0];
+
+            const mainNews = newsSingle.appendChild(document.createElement('div'));
+            mainNews.classList.add('main-news');
+            // To keep main-news hidden
+            mainNews.classList.add('main-news-inactive');
+            mainNews.innerHTML = newsList[i].news[1];
+
+            // To make main-news visible
+            // mainNews.classList.remove('main-news-inactive');
+        }
+
+        // showNewsOfDate(generateId(date, date.getDate()));
+    }
+}
+
 renderCalendar();
 renderNewsList();
